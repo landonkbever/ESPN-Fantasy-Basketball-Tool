88a2d02cabc9f7ce0a2f212d24168ab9cc0f7845
Landon Bever <landonkb23@gmail.com>
2022-10-21 10:21:56 -0500

    connected league

--- fbtmain.py
+++ fbtmain.py
@@ -1,3 +1,6 @@
 #ESPN Fantasy Basketball tool
 #Used to take grown men's money
 
+from espn_api.basketball import League
+league = League(league_id=1330860437, year=2023, espn_s2='AEC2JFptBpZayXb6oywXx6ANqklaYZPAt03zUEx7Mc141AE6mYOcOSvnFr3CWhhGWuYIRThu1eYKb1kVLdNoKP4RSyLXqGJQR2eswKnhyySWRRkavf%2BakY6jgs0bcJHNPECxcvI3eUNYZTWpfpL9bBQEE4ytzQDDn%2FiqHq%2BKywWIXhqVNgKYWFKHguoOH6lbtqORRLer%2FbtEXJTSE9HyNbmm7U4%2BypY3%2F9A3FYr4eqiMc6%2BOEzlT7ubGXnnNKO%2FVRa1BR8VhuokgICHheMKaZV2P', swid='{2EA153B1-AE28-425A-832F-5BBD45F22D80}')
+
